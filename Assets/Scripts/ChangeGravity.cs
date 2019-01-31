6ab708356cb64b6448e603e94ad838574a948bd1
Vitaki <nathan.mendes@hotmail.fr>
2019-01-31 19:26:52 +0100

    script changement de gravité

---
+++ Assets/Scripts/ChangeGravity.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeGravity : MonoBehaviour {
+
+    public Vector3 vectorgravity;
+
+    // Use this for initialization
+    void Start () {
+        Physics.gravity = new Vector3(0, -10f, 0);
+        vectorgravity = Physics.gravity;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            vectorgravity = vectorgravity + new Vector3(0,2f,0);
+            Physics.gravity = vectorgravity;
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            vectorgravity = vectorgravity + new Vector3(0, -2f, 0);
+            Physics.gravity = vectorgravity;
+        }
+    }
+
+    void Gravity ()
+    {
+        Physics.gravity = vectorgravity;
+    }
+}
