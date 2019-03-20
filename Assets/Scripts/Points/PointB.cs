f0d2e91f54cd373f1b3f724969f9833143b3ef07
Gregzzou <gregoire.routier@outlook.fr>
2019-03-20 13:57:59 +0100

    Les points

---
+++ Assets/Scripts/Points/PointB.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointB : MonoBehaviour
+{
+    int ptsB = 500000;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(90 * Time.deltaTime, 90 * Time.deltaTime, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            other.GetComponent<Player>().points = other.GetComponent<Player>().points + ptsB;
+            //add 1 points
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Points/PointC.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointC : MonoBehaviour
+{
+    int ptsC = 1000000;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(90 * Time.deltaTime, 90 * Time.deltaTime, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            other.GetComponent<Player>().points = other.GetComponent<Player>().points + ptsC;
+            //add 1 points
+            Destroy(gameObject);
+        }
+    }
+}
